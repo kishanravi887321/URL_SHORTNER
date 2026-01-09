e02161d32dada5d94b5224c66607e61dfa4b47d1
Ravi Kishan <kishanravi887321@gmail.com>
2026-01-10 02:14:18 +0530

    Set up Express server and define mainShort routes for URL shortening

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,10 @@
 import express from "express";
+import {router as mainShortRoutes} from "./routes/mainShort.routes.js";
 
 const app = express();
+app.use(express.json());
+
+app.use("/api", mainShortRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
---
+++ backend/src/routes/mainShort.routes.js
@@ -0,0 +1,10 @@
+import { getOriginalUrl } from "../controllers/mainShort.controllers.js";
+import { createShortUrl } from "../controllers/mainShort.controllers.js";
+import express from "express";
+
+const router = express.Router();
+
+router.post("/shorten", createShortUrl);
+router.get("/:shortUrl", getOriginalUrl);
+
+export { router };
